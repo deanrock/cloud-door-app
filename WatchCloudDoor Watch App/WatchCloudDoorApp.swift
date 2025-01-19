219397a7b7ff4fce4771332c8dad2d18ee9958c6
Dejan Levec <dejan@dejanlevec.com>
2025-01-19 12:15:47 +0100

    add new target

---
+++ WatchCloudDoor Watch App/WatchCloudDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCloudDoorApp.swift
+//  WatchCloudDoor Watch App
+//
+//  Created by Dejan Levec on 19. 1. 25.
+//
+
+import SwiftUI
+
+@main
+struct WatchCloudDoor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchCloudDoor Watch AppTests/WatchCloudDoor_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchCloudDoor_Watch_AppTests.swift
+//  WatchCloudDoor Watch AppTests
+//
+//  Created by Dejan Levec on 19. 1. 25.
+//
+
+import Testing
+@testable import WatchCloudDoor_Watch_App
+
+struct WatchCloudDoor_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
