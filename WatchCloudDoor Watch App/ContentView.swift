3a9fce699cb0856f96a0718f6b32f53a15602712
Dejan Levec <dejan@dejanlevec.com>
2025-04-13 08:32:52 +0200

    add default text

--- WatchCloudDoor Watch App/ContentView.swift
+++ WatchCloudDoor Watch App/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Please configure credentials on iPhone app.")
         }
         .padding()
     }
