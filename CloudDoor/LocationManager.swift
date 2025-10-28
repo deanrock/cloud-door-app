19fc1da80129c10cce2bba160effd0a85e6db5ee
Dejan Levec <dejan@dejanlevec.com>
2025-10-28 07:10:14 +0100

    poc

--- CloudDoor/LocationManager.swift
+++ CloudDoor/LocationManager.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreLocation
+import UserNotifications
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     let manager = CLLocationManager()
@@ -16,7 +17,65 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         super.init()
         manager.delegate = self
         manager.startUpdatingLocation()
-        manager.requestWhenInUseAuthorization()
+        manager.requestAlwaysAuthorization()
+        self.monitor()
+    }
+    
+    func monitor() {
+        
+        Task {
+            let center = UNUserNotificationCenter.current()
+            
+            
+            do {
+                try await center.requestAuthorization(options: [.alert, .sound, .badge])
+                print("yes permissions notif")
+            } catch {
+                // Handle the error here.
+                print("nope permissions notif")
+            }
+        }
+        if CLLocationManager.isMonitoringAvailable(for:
+                                                    CLBeaconRegion.self) {
+            
+            
+            // Create the region and begin monitoring it.
+            let region = CLBeaconRegion(uuid: UUID.init(uuidString: "c29ce823-e67a-4e71-bff2-abaa32e77a98")!, identifier: "beacon")
+            let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
+            trigger.region.notifyOnEntry = true
+            trigger.region.notifyOnExit = false
+            
+            let content = UNMutableNotificationContent()
+            content.title = "title"
+            content.body = "Body"
+            content.interruptionLevel = .active
+
+            
+            let request = UNNotificationRequest(identifier: "X", content: content, trigger: trigger)
+
+            // Add the request
+            let center = UNUserNotificationCenter.current()
+            center.add(request, withCompletionHandler: { (error) in
+                 if let error = error {
+                      print("\n\t ERROR: \(error)")
+                 } else {
+                      print("\n\t request fulfilled \(request)")
+                 }
+            })
+            
+            print("AVAILABLE")
+            
+            self.manager.startMonitoring(for: region)
+            self.manager.startRangingBeacons(in: region)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+            print("proximity", beacon.proximity)
+        } else {
+            print("nope?")
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
